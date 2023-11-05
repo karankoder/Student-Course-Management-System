f466ea855ec5c437c5f49c7d928ef169fe366120
karankoder <karankr.das.cse22@iitbhu.ac.in>
2023-11-05 23:43:09 +0530

    Home template added

--- Course_management_system/settings.py
+++ Course_management_system/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'Course_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
